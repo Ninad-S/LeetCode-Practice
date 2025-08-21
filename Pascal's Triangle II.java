eeba56d93b5fe541024d68420d529e7a7be93915
Ninad-S <144244166+Ninad-S@users.noreply.github.com>
2025-08-21 12:26:40 -0500

    Create Pascal's Triangle II.java

---
+++ Pascal's Triangle II.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows,int i, List<List<Integer>> rows)
+    {
+        if (i==numRows)
+        {
+            return rows;
+        }
+        rows.add(new ArrayList<>());
+        rows.get(i).add(1);
+        rows.get(i).add(1);
+        ArrayList<Integer> sums = new ArrayList<>();
+        for (int j = 0; j < rows.get(i-1).size()-1; j++)
+        {
+            sums.add(rows.get(i-1).get(j) + rows.get(i-1).get(j+1));
+        } 
+        for (int j = 0; j < sums.size(); j++)
+        {
+            rows.get(i).add(j+1,sums.get(j));
+        }
+        return generate(numRows,i+1,rows);
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> rows = new ArrayList<>();
+        rows.add(new ArrayList<>());
+        rows.get(0).add(1);
+        if (numRows==1)
+        {
+            return rows;
+        }
+        rows.add(new ArrayList<>());
+        rows.get(1).add(1); rows.get(1).add(1);
+        if (numRows==2)
+        {
+            return rows;
+        }
+        return generate(numRows,2,rows);
+        
+    }
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> rows = generate(rowIndex+1);
+        return rows.get(rowIndex);
+    }
+}
